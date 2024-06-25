daa61a5b501eeec1aa5df45aa949ab56301befab
allefgoncalves <allefgoncalves@gmail.com>
2024-06-25 18:07:58 -0300

    adicionando o app

--- zaprecall/src/App.jsx
+++ zaprecall/src/App.jsx
@@ -1,35 +1,66 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import styled from 'styled-components';
+import Questions from './Questions';
+import logo from './assets/logo.png';
+import CARDS from './CARDS';
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [count, setcount] = useState(0);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Container>
+      <Logo>
+        <img src={logo}/>
+        <div>ZapRecall</div>
+      </Logo>
+      <Questions CARDS={CARDS} count={count} setcount={setcount}/>
+      <Baseboard>
+        {count}/{CARDS.length}
+      </Baseboard>
+    </Container>
+  );
 }
 
-export default App
+const Baseboard =styled.div`
+  width: 375px;
+  height: 70px;
+  background-color: #FFFF;
+  border: 1px solid #dbd8d8 ;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  box-shadow: 0px -4px 6px 0px #0000000D;
+
+`
+
+const Container = styled.div`
+  background-color:#FB6B6B;
+  width: 375px;
+  height: auto;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  margin: 0px auto;
+`
+const Logo =styled.div`
+  width: 255px;
+  height: 60px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 40px auto;
+
+  color: #FFFFFF;
+  font-family: Righteous;
+  font-size: 50px;
+  font-weight: 800;
+  line-height: 45px;
+  letter-spacing: -0.012em;
+  text-align: center;
+  
+  img{
+    width: 52px;
+    height: 60px;
+  }
+ `
---
+++ zaprecall/src/Questions.jsx
@@ -0,0 +1,13 @@
+import seta_play from './assets/seta_play.png';
+import seta_virar from './assets/seta_virar.png';
+import styled from 'styled-components';
+import { useState } from 'react';
+import erro from './assets/icone_erro.png';
+import quase from './assets/icone_quase.png';
+import certo from './assets/icone_certo.png';
+
+export default function Perguntas({CARDS, count, setcount}) {
+    return(`
+
+    `);
+}
